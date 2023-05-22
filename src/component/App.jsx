079fcf555c835de729335d1de4b683f11c1179fa
Tejas Musale <tejasmusale99@gmail.com>
2023-05-22 19:14:43 +0530

    input component

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from "./Header"
 import "./index.css"
+import Input from './Input';
 
 function App() {
   return (
-    <div>
+    <div className='Container'>
       <Header />
+      <Input />
     </div>
   );
 }
---
+++ src/component/Input.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Input = () => {
+  return <div>
+    <form>
+    <input type="text" placeholder="Add a items" />
+    <button>+</button>
+    </form>
+  </div>;
+};
+export default Input;
